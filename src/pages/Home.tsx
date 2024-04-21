8b3846414245193b0669f2d2a076b311c8dd3439
Filip Burski <92650529+FlickJS@users.noreply.github.com>
2024-04-21 18:38:46 +0200

    fixed imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,8 @@ import React from "react";
 import styles from "./Home.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faFaceGrin,
   faFaceGrinStars,
   faRoad,
-  faUserCog,
   faWrench,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
